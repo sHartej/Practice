ad911e269ebc3d31f9d0a6d6e5a53c8b24577166
sHartej <168448483+sHartej@users.noreply.github.com>
2026-05-28 14:41:23 +0200

    Add files via upload

---
+++ gif_creator.py
@@ -0,0 +1,10 @@
+import imageio.v3 as iio
+
+filenames = ['team-pic1.png', 'team-pic2.png']
+images = [ ]
+
+for filename in filenames:
+    images.append(iio.imread(filenames))
+
+iio.imwrite('team.gif', images, duration = 500, loop = 0)
+
